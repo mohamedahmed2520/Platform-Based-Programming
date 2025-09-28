57a2e8a28dfbe1959077b76e2a155da6e9ec0aec
Testtestt2000 <testmoah2000@gmail.com>
2025-09-28 20:10:08 +0700

    Upload all folders and files

---
+++ week 2/simple_math_with_variables.js
@@ -0,0 +1,10 @@
+let a = 8;
+let b = 3;
+
+let sum = a + b;           // 11
+let product = a * b;       // 24
+let average = (a + b) / 2; // 5.5
+
+console.log("sum =", sum);
+console.log("product =", product);
+console.log("average =", average);
